f1aeb834a883761468a8e5a60ff143ecc36215a9
Ghyghi <nagasaroghislaine3@gmail.com>
2024-03-02 12:06:46 +0200

    ok

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
+
 """Script that fetch 10 hot post for a given subreddit."""
+
 import requests
 
 
 def number_of_subscribers(subreddit):
+
     """Return the number of subscribers for the given subreddit."""
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {'User-Agent': 'My User Agent 1.0'}
